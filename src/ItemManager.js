c701c387031b0d08e29800eb0def3dcb2af47e5f
GordonDHuang <gordondhuang@gmail.com>
2022-02-05 17:29:56 -0800

    managers

--- src/ItemManager.js
+++ src/ItemManager.js
@@ -1,10 +1,12 @@
 var button = document.querySelector('.inventory-submit-button')
 
-button.addEventListener('click', (name, numItems) => {
+button.addEventListener('click', (name, numItems, quantity, expiration, price ) => {
     let newItem = {
         name: name,
         numItems: numItems,
-        items:[]
+        quantity: quantity,
+        expiration: expiration,
+        price: price
     }
     localStorage.setItem(name, JSON.stringify(newItem))
 })
